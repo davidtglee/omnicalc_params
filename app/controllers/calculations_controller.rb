00e27848479872c253d19f42683bf4fad7f2c6ca
David Lee <david.tg.lee@gmail.com>
2017-04-28 06:30:58 -0500

    final

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -24,34 +24,45 @@ def square_form
 end
 
 def square
-  @user_number=params[:user1_number].to_f
-  @squared_number=@user_number**2
+
+
+  @user_provided_number= params[:user_input]
+  @squared_number= @user_provided_number.to_f ** 2.round(2)
   render("calculations/square.html.erb")
 end
 
+
 def square_root_form
-  render("calculations/square_root_form.html.erb")
-end
+    render("calculations/square_root_form.html.erb")
+  end
 
-def square_root
-  @user_number=params[:user2_number].to_f
-  @sqrt_number=Math.sqrt(@user_number)
-  render("calculations/square_root.html.erb")
-end
+  def square_root
+    @user_provided_number= params[:user_input]
+    @squared_root_number= (Math.sqrt(@user_provided_number.to_f)).round(3)
+    render("calculations/square_root.html.erb")
+  end
 
-def payment_form
-  render("calculations/payment_form.html.erb")
-end
+  def payment_form
+    render("calculations/payment_form.html.erb")
+  end
 
-def payment
-  @interest=params[:user_interest].to_f/100
-  monthlyrate=@interest/1200
-  @years=params[:user_year].to_f
-  months=@years*12
-  @principle=params[:user_principle].to_f
-  @payment=(monthlyrate*@principle)/(1-(1+monthlyrate)**(-months))
-  render("calculations/payment.html.erb")
-end
+
+  def payment_form
+      render("calculations/payment_form.html.erb")
+    end
+
+
+    def payment
+      @apr = params["user_rate"].to_f
+      @years = params["user_years"].to_i
+      @principal = params["user_principal"].to_f
+
+      @rate=@apr/1200
+      payments=@years*12
+      @monthly_payment = (@rate*@principal/(1-(1+@rate)**(-payments))).round(2)
+      render("calculations/payment.html.erb")
+    end
+
 
 def random_form
   render("calculations/random_form.html.erb")
