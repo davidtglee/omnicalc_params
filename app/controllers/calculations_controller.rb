8d661c87193ee1f44e2db650560bfa802fbca144
David Lee <david.tg.lee@gmail.com>
2017-04-28 04:54:38 -0500

    prelim_submit

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -11,10 +11,12 @@ def flex_squarert
     render("calculations/flex_squareroot.html.erb")
 end
     def flex_payments
-      @interest=params[:interest].to_f/10000
-      @years=params[:year].to_f
-      @principle=params[:principle].to_f
-      @payment=@interest*@principle/(1-(1+@principle)**(-@years))
+      @interest=params[:interest].to_f/100
+      monthlyrate=@interest/1200
+      @years=params[:year].to_i
+      months=@years*12
+      @principle=params[:principle].to_i
+      @payment=(monthlyrate*@principle)/(1-(1+monthlyrate)**(-months))
   render("calculations/flex_payment.html.erb")
 end
 def square_form
@@ -25,6 +27,42 @@ def square
   @user_number=params[:user_number].to_f
   @squared_number=@user_number**2
   render("calculations/square.html.erb")
+end
+
+def square_root_form
+  render("calculations/square_root_form.html.erb")
+end
+
+def square_root
+  @user_number=params[:users_number].to_f
+  @sqrt_number=Math.sqrt(@user_number)
+  render("calculations/square_root.html.erb")
+end
+
+def payment_form
+  render("calculations/payment_form.html.erb")
+end
+
+def payment
+  @interest=params[:user_interest].to_f/100
+  monthlyrate=@interest/1200
+  @years=params[:user_year].to_f
+  months=@years*12
+  @principle=params[:user_principle].to_f
+  @payment=(monthlyrate*@principle)/(1-(1+monthlyrate)**(-months))
+  render("calculations/payment.html.erb")
+end
+
+def random_form
+  render("calculations/random_form.html.erb")
+end
+
+
+def random
+  @minimum = params[:minimum]
+  @maximum = params[:maximum]
+  @random_number= rand(@minimum.to_f..@maximum.to_f)
+  render("calculations/random.html.erb")
 end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   get("flexible/square/:the_number", {:controller=>"calculations", :action=>"flex_square"})
-  get("flexible/squarert/:the_number", {:controller=>"calculations", :action=>"flex_squarert"})
+  get("flexible/square_root/:the_number", {:controller=>"calculations", :action=>"flex_squarert"})
   get("flexible/payment/:interest/:year/:principle", {:controller=>"calculations", :action=>"flex_payments"})
-  get("square/form", {:controller=>"calculations", :action=>"square_form"})
+  get("square/new", {:controller=>"calculations", :action=>"square_form"})
   get("square/results", {:controller=>"calculations", :action=>"square"})
+  get("square_root/new", {:controller=>"calculations", :action=>"square_root_form"})
+  get("square_root/results", {:controller=>"calculations", :action=>"square_root"})
+  get("payment/new", {:controller=>"calculations", :action=>"payment_form"})
+  get("payment/results", {:controller=>"calculations", :action=>"payment"})
+  get("random/new", {:controller=>"calculations", :action=>"random_form"})
+  get("random/results", {:controller=>"calculations", :action=>"random"})
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
